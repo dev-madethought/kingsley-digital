35ba7e83ad7bbd69e54021a12d64d36f5a2168db
dev-madethought <dev@madethought.com>
2024-04-17 19:51:23 -0400

    update

--- src/containers/cookies/index.tsx
+++ src/containers/cookies/index.tsx
@@ -25,7 +25,6 @@ export const Cookies = () => {
     const timeout = setTimeout(() => {
       if (!savedCookie) {
         setIsVisible(true)
-        console.log("HERE", savedCookie)
       }
     }, 1000)
     return () => {
