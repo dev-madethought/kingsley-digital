b4633e999938a8daa9f96ccd2deebf4ed0a90bba
dev-madethought <dev@madethought.com>
2024-04-17 19:49:02 -0400

    fix

--- src/containers/cookies/index.tsx
+++ src/containers/cookies/index.tsx
@@ -7,9 +7,7 @@ import { PortableText } from "@portabletext/react"
 
 import { Box } from "@/components/box"
 import { Button } from "@/components/button"
-import { Container } from "@/components/container"
-import { getWidth, Grid } from "@/components/grid"
-import { useDebug } from "@/components/grid"
+import { getWidth } from "@/components/grid"
 import { RootState } from "@/state/store"
 
 import { components } from "./components"
@@ -18,19 +16,17 @@ import { getCookiesCTA, getCookiesMessage } from "./translations"
 export const Cookies = () => {
   const settings = useSelector((state: RootState) => state.global.settings)
   const language = useSelector((state: RootState) => state.global.language)
-  const [cookieValue, setCookieValue] = useState<string | null>(null)
   const [isVisible, setIsVisible] = useState(false)
-  const { boxShadow } = useDebug()
   const cookies = new Cookie()
 
   useEffect(() => {
     const savedCookie = cookies.get("userAcceptedCookies")
-    if (savedCookie) {
-      setCookieValue(savedCookie)
-    }
 
     const timeout = setTimeout(() => {
-      setIsVisible(true)
+      if (!savedCookie) {
+        setIsVisible(true)
+        console.log("HERE", savedCookie)
+      }
     }, 1000)
     return () => {
       clearTimeout(timeout)
@@ -40,20 +36,8 @@ export const Cookies = () => {
 
   const handleAccept = () => {
     cookies.set("userAcceptedCookies", "true", { path: "/" })
-    setCookieValue("true")
+    setIsVisible(false)
   }
-
-  if (cookieValue) {
-    return null
-  }
-
-  /*
- <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: isVisible ? 1 : 0 }}
-      transition={{ duration: 0.25 }}
-    >
-  */
 
   return (
     <AnimatePresence>
@@ -66,28 +50,35 @@ export const Cookies = () => {
             width: getWidth(6),
           }}
         >
-          <Box
-            css={{
-              width: "100%",
-              padding: 20,
-              backgroundColor: "$darker",
-              borderRadius: 5,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-              alignSelf: "end",
-              // border: "1px solid $background",
-            }}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5 }}
+            style={{ width: "100%" }}
           >
-            <PortableText
-              value={getCookiesMessage(language, settings?.cookies) as any}
-              components={components}
-            />
+            <Box
+              css={{
+                width: "100%",
+                padding: 20,
+                backgroundColor: "$darker",
+                borderRadius: 5,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                alignSelf: "end",
+              }}
+            >
+              <PortableText
+                value={getCookiesMessage(language, settings?.cookies) as any}
+                components={components}
+              />
 
-            <Button onClick={handleAccept}>
-              {getCookiesCTA(language, settings?.cookies)}
-            </Button>
-          </Box>
+              <Button onClick={handleAccept}>
+                {getCookiesCTA(language, settings?.cookies)}
+              </Button>
+            </Box>
+          </motion.div>
         </Box>
       )}
     </AnimatePresence>
