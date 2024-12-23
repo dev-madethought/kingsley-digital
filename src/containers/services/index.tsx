9230f6bd6bb1eb99a30d400318f958b497fd4f3c
el-schneider <mail@jonaslist.de>
2024-12-23 16:03:17 +0100

    fix: revert and change less

--- src/containers/services/index.tsx
+++ src/containers/services/index.tsx
@@ -8,7 +8,6 @@ import { PortableText } from "@portabletext/react"
 import { AnimationFadeIn } from "@/components/animation-fade-in"
 import { AnimationMaskReveal } from "@/components/animation-mask-reveal"
 import { Box } from "@/components/box"
-// import { Button } from "@/components/button"
 import { Container } from "@/components/container"
 import { Grid, useDebug } from "@/components/grid"
 import Heading from "@/components/heading"
@@ -19,226 +18,110 @@ import { urlForImage } from "@/sanity/lib/image"
 import { setService } from "@/state/reducers/service"
 import { RootState } from "@/state/store"
 import { Services as ServicesProps } from "@/types/sanity"
-import { otherLanguage } from "@/utils"
 
 import * as Styles from "./styles"
-import {
-  // getLearnMoreButton,
-  getServiceDescription,
-  getServiceTitle,
-} from "./translations"
+import { getServiceDescription, getServiceTitle } from "./translations"
 
-// New type definitions
-type ServiceImage = {
-  _key: string
-  images?: string[]
-}
+const LAYOUTS = ["layout1"]
 
-// Custom hook for service management
-const useServiceManagement = (props: ServicesProps) => {
+export const Services = (props: ServicesProps) => {
   const dispatch = useDispatch()
   const language = useSelector((state: RootState) => state.global.language)
-  const [activeKey, setActiveKey] = useState<string | undefined>()
-
-  const handleServiceChange = (value: string) => {
-    const service = props.allServices?.find((s) => s._key === value)
-    if (service) {
-      dispatch(setService({ ...service }))
-      setActiveKey(value === activeKey ? undefined : value)
-    }
-  }
-
-  return {
-    activeKey,
-    language,
-    handleServiceChange,
-  }
-}
-
-// Image layout components
-const DefaultImage = ({ image }: { image: any }) => (
-  <Box
-    css={{
-      position: "relative",
-      alignItems: "flex-start",
-      column: 9,
-      img: {
-        width: "100%",
-        height: "auto",
-        objectFit: "contain",
-      },
-    }}
-  >
-    <AnimationMaskReveal>
-      <Image src={urlForImage(image)} alt="default" width={732} height={556} />
-    </AnimationMaskReveal>
-  </Box>
-)
-
-const ServiceImages = ({ images }: { images: string[] }) => (
-  <Box css={{ gap: 10 }}>
-    <Box
-      css={{
-        column: 7,
-        position: "relative",
-        alignItems: "flex-start",
-        img: {
-          width: "100%",
-          height: "auto",
-          objectFit: "contain",
-          objectPosition: "top",
-        },
-      }}
-    >
-      {images[0] && (
-        <AnimationMaskReveal>
-          <Image src={images[0]} alt="image 1" width={504} height={756} />
-        </AnimationMaskReveal>
-      )}
-    </Box>
-
-    <Box
-      desktop
-      css={{
-        column: 6,
-        position: "relative",
-        alignItems: "flex-start",
-        img: {
-          width: "100%",
-          height: "auto",
-          objectFit: "contain",
-          objectPosition: "top",
-        },
-      }}
-    >
-      {images[1] && (
-        <AnimationMaskReveal delay={0.125}>
-          <Image src={images[1]} alt="image 2" width={275} height={200} />
-        </AnimationMaskReveal>
-      )}
-    </Box>
-  </Box>
-)
-
-// Accordion item component
-const ServiceAccordionItem = ({
-  service,
-  index,
-  language,
-}: {
-  service: any
-  index: number
-  language: string
-}) => (
-  <Styles.AccordionItem value={service._key}>
-    <Styles.AccordionTrigger>
-      <Box css={{ gap: 10 }}>
-        <Box css={{ column: 1, paddingLeft: 8, alignItems: "center" }}>
-          {String(index + 1).padStart(2, "0")}
-        </Box>
-        <Text headingS>{getServiceTitle(language, service)}</Text>
-      </Box>
-      <Box
-        css={{
-          position: "relative",
-          alignItems: "center",
-          justifyContent: "flex-end",
-          width: 24,
-          height: 24,
-
-          "&:before": {
-            content: '""',
-            position: "absolute",
-            top: "50%",
-            right: 6,
-            width: 13,
-            height: 1,
-            background: "$typography",
-            transition: "all 300ms ease-out",
-          },
-
-          "&:after": {
-            content: '""',
-            position: "absolute",
-            right: 12,
-            width: 1,
-            height: 13,
-            background: "$typography",
-            opacity: 1,
-            transition: "all 300ms ease-out",
-          },
-
-          "[data-state=open] &": {
-            "&:before": {
-              transform: "rotate(180deg)",
-            },
-
-            "&:after": {
-              opacity: 0,
-              transform: "rotate(270deg)",
-            },
-          },
-        }}
-      />
-    </Styles.AccordionTrigger>
-    <Styles.AccordionContent>
-      <Box css={{ column: 1 }} />
-      <Box
-        css={{
-          column: 8,
-          flexDirection: "column",
-          gap: 24,
-          paddingTop: 24,
-          paddingBottom: 64,
-
-          "@tablet": {
-            column: 12,
-            paddingBottom: 48,
-          },
-
-          "@desktop": {
-            column: 8,
-            paddingBottom: 64,
-          },
-        }}
-      >
-        <AnimationFadeIn>
-          <Box>
-            <PortableText
-              value={getServiceDescription(language, service) as any}
-              components={components}
-            />
-          </Box>
-        </AnimationFadeIn>
-        <AnimationFadeIn>
-          <Box css={{ opacity: 0.5 }}>
-            <PortableText
-              value={
-                getServiceDescription(otherLanguage(language), service) as any
-              }
-              components={components}
-            />
-          </Box>
-        </AnimationFadeIn>
-      </Box>
-    </Styles.AccordionContent>
-  </Styles.AccordionItem>
-)
-
-// Main component
-export const Services = (props: ServicesProps) => {
+  const settings = useSelector((state: RootState) => state.global.settings)
+  const [index, setIndex] = useState(-1)
   const { debug, boxShadow } = useDebug()
-  const { activeKey, language, handleServiceChange } =
-    useServiceManagement(props)
 
-  const renderImageLayout = () => {
-    if (!activeKey) {
-      return <DefaultImage image={props.image} />
+  const handleChange = (value: string) => {
+    const service = props.allServices?.find((s) => s._key === value)
+    dispatch(setService({ ...service }))
+
+    const nextIndex = props.allServices?.findIndex((s) => s._key === value)
+    setIndex(nextIndex === undefined ? -1 : nextIndex)
+  }
+
+  console.log(index)
+
+  const getImageLayout = (index: number) => {
+    // Return default generic image when no service is selected
+    if (index === -1) {
+      return (
+        <Box
+          css={{
+            position: "relative",
+            alignItems: "flex-start",
+            column: 12,
+
+            img: {
+              width: "100%",
+              height: "auto",
+              objectFit: "contain",
+            },
+          }}
+        >
+          <AnimationMaskReveal>
+            <Image
+              src={urlForImage(props.image)}
+              alt="default"
+              width={504}
+              height={756}
+            />
+          </AnimationMaskReveal>
+        </Box>
+      )
     }
 
-    const activeService = props.allServices?.find((s) => s._key === activeKey)
-    const images = activeService?.images?.map((i) => urlForImage(i)) || []
-    return <ServiceImages images={images} />
+    const images =
+      props.allServices?.[index]?.images?.map((i) => urlForImage(i)) || []
+    const key = props.allServices?.[index]?._key
+    const image1 = images?.[0]
+    const image2 = images?.[1]
+
+    return (
+      <Box css={{ gap: 10 }} key={key}>
+        <Box
+          css={{
+            column: 7,
+            position: "relative",
+            alignItems: "flex-start",
+
+            img: {
+              width: "100%",
+              height: "auto",
+              objectFit: "contain",
+              objectPosition: "top",
+            },
+          }}
+        >
+          {image1 && (
+            <AnimationMaskReveal>
+              <Image src={image1} alt="image 1" width={504} height={756} />
+            </AnimationMaskReveal>
+          )}
+        </Box>
+
+        <Box
+          desktop
+          css={{
+            column: 6,
+            position: "relative",
+            alignItems: "flex-start",
+
+            img: {
+              width: "100%",
+              height: "auto",
+              objectFit: "contain",
+              objectPosition: "top",
+            },
+          }}
+        >
+          {image2 && (
+            <AnimationMaskReveal delay={0.125}>
+              <Image src={image2} alt="image 2" width={275} height={367} />
+            </AnimationMaskReveal>
+          )}
+        </Box>
+      </Box>
+    )
   }
 
   return (
@@ -254,19 +137,16 @@ export const Services = (props: ServicesProps) => {
         },
       }}
     >
-      <Grid
-        css={{
-          rowGap: 40,
-        }}
-      >
+      <Grid css={{ rowGap: "$space$40" }}>
         <Heading props={props} language={language} translationKey="title" />
+
         <TranslatedRow props={props} translationKey="description" />
 
-        {/* Images Section */}
+        {/* IMAGES */}
         <Box
           css={{
             gridColumn: "span 10",
-            marginTop: 56,
+            marginTop: 64,
             marginBottom: 48,
 
             "@tablet": {
@@ -280,10 +160,10 @@ export const Services = (props: ServicesProps) => {
             },
           }}
         >
-          <AnimatePresence>{renderImageLayout()}</AnimatePresence>
+          <AnimatePresence>{getImageLayout(index)}</AnimatePresence>
         </Box>
 
-        {/* Accordion Section */}
+        {/* ACCORDION */}
         <Box
           css={{
             gridColumn: "span 12",
@@ -291,27 +171,114 @@ export const Services = (props: ServicesProps) => {
 
             "@tablet": {
               gridColumn: "10 / span 15",
-              marginTop: 56,
+              marginTop: 64,
             },
 
             "@desktop": {
               gridColumn: "15 / span 10",
-              marginTop: 56,
+              marginTop: 64,
             },
           }}
         >
           <Styles.AccordionRoot
             type="single"
             collapsible
-            onValueChange={handleServiceChange}
+            onValueChange={handleChange}
           >
             {props.allServices?.map((service, i) => (
-              <ServiceAccordionItem
-                key={service._key}
-                service={service}
-                index={i}
-                language={language}
-              />
+              <Styles.AccordionItem key={service._key} value={service._key}>
+                <Styles.AccordionTrigger>
+                  <Box css={{ gap: 10 }}>
+                    <Box
+                      css={{
+                        column: 2,
+                        paddingLeft: 8,
+                        alignItems: "center",
+                      }}
+                    >
+                      0{i + 1}
+                    </Box>
+                    <Text headingS>{getServiceTitle(language, service)}</Text>
+                  </Box>
+                  <Box
+                    css={{
+                      position: "relative",
+                      // column: 1,
+                      // paddingRight: 8,
+                      alignItems: "center",
+                      justifyContent: "flex-end",
+                      width: 24,
+                      height: 24,
+
+                      "&:before": {
+                        content: '""',
+                        position: "absolute",
+                        top: "50%",
+                        right: 6,
+                        width: 13,
+                        height: 1,
+                        background: "$typography",
+                        transition: "all 300ms ease-out",
+                      },
+
+                      "&:after": {
+                        content: '""',
+                        position: "absolute",
+                        right: 12,
+                        width: 1,
+                        height: 13,
+                        background: "$typography",
+                        opacity: 1,
+                        transition: "all 300ms ease-out",
+                      },
+
+                      "[data-state=open] &": {
+                        "&:before": {
+                          transform: "rotate(180deg)",
+                        },
+
+                        "&:after": {
+                          opacity: 0,
+                          transform: "rotate(270deg)",
+                        },
+                      },
+                    }}
+                  />
+                </Styles.AccordionTrigger>
+                <Styles.AccordionContent>
+                  <Box css={{ column: 1 }} />
+                  <Box
+                    css={{
+                      column: 8,
+                      flexDirection: "column",
+                      gap: 24,
+                      paddingTop: 24,
+                      paddingBottom: 64,
+
+                      "@tablet": {
+                        column: 12,
+                        paddingBottom: 48,
+                      },
+
+                      "@desktop": {
+                        column: 8,
+                        paddingBottom: 64,
+                      },
+                    }}
+                  >
+                    {/* {getServiceSinopsis(language, service)} */}
+                    <AnimationFadeIn>
+                      <PortableText
+                        value={getServiceDescription(language, service) as any}
+                        components={components}
+                      />
+                    </AnimationFadeIn>
+                    {/* <Button onClick={handleLearnMore}>
+                      {getLearnMoreButton(language, settings?.buttons)}
+                    </Button> */}
+                  </Box>
+                </Styles.AccordionContent>
+              </Styles.AccordionItem>
             ))}
           </Styles.AccordionRoot>
         </Box>
